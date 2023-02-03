7edde6e2536020d11d7a671116fe827fa12e3b60
DaeHyeonHong <fholng@outlook.com>
2023-02-04 00:29:35 +0900

    pojo -> spring boot test

--- src/test/java/com/example/productorderservice/product/ProductServiceTest.java
+++ src/test/java/com/example/productorderservice/product/ProductServiceTest.java
@@ -1,40 +1,35 @@
 package com.example.productorderservice.product;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
-@ExtendWith(value = MockitoExtension.class)
+@SpringBootTest
 class ProductServiceTest {
-    private ProductPort productPort;
+    @Autowired
     private ProductService productService;
-
-    @BeforeEach
-    void setUp() {
-        this.productPort = Mockito.mock(ProductPort.class);
-        this.productService = new ProductService(productPort);
-    }
 
     @Test
     void 상품수정() {
+        this.productService.addProduct(ProductSteps.상품등록요청_생성());
+
         final Long productId = 1L;
         final String afterName = "상품수정";
         final int afterPrice = 2_000;
-        final UpdateProductRequest request = new UpdateProductRequest(afterName, afterPrice, DiscountPolicy.NONE);
         final int beforePrice = 1_000;
         final String beforeName = "상품평";
-        final Product product = new Product(beforeName, beforePrice, DiscountPolicy.NONE);
-        Mockito.when(productPort.getProduct(productId)).thenReturn(product);
+        final UpdateProductRequest request = new UpdateProductRequest(afterName, afterPrice, DiscountPolicy.NONE);
 
         this.productService.updateProduct(productId, request);
 
-        Assertions.assertEquals(product.getName(), afterName);
-        Assertions.assertEquals(product.getPrice(), afterPrice);
-        Assertions.assertNotEquals(product.getName(), beforeName);
-        Assertions.assertNotEquals(product.getPrice(), beforePrice);
+        final ResponseEntity<GetProductResponse> response = this.productService.getProduct(productId);
+        final GetProductResponse productResponse = response.getBody();
+        Assertions.assertEquals(productResponse.name(), afterName);
+        Assertions.assertEquals(productResponse.price(), afterPrice);
+        Assertions.assertNotEquals(productResponse.name(), beforeName);
+        Assertions.assertNotEquals(productResponse.price(), beforePrice);
     }
 
 }
