3a851c31102b7972a6aba21a4460602f9a398b9c
DaeHyeonHong <fholng@outlook.com>
2023-02-09 18:32:44 +0900

    sppringtest -> apitest

--- src/main/java/com/example/productorderservice/order/OrderService.java
+++ src/main/java/com/example/productorderservice/order/OrderService.java
@@ -1,9 +1,15 @@
 package com.example.productorderservice.order;
 
 import com.example.productorderservice.product.Product;
-import org.springframework.stereotype.Component;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
+@RequestMapping(value = "/orders")
 class OrderService {
     private final OrderPort orderPort;
 
@@ -11,10 +17,12 @@ class OrderService {
         this.orderPort = orderPort;
     }
 
-    public void createOrder(final CreateOrderRequest request) {
+    @PostMapping
+    public ResponseEntity<Void> createOrder(@RequestBody final CreateOrderRequest request) {
         final Product product = this.orderPort.getProductById(request.productId());
         final Order order = new Order(product, request.quantity());
 
         this.orderPort.save(order);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
