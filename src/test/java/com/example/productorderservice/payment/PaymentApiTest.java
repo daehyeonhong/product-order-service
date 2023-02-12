3e106ad48b73fc213a7f1d5fafadc7f42d163b67
DaeHyeonHong <fholng@outlook.com>
2023-02-12 12:09:43 +0900

    spring to api test mk1

---
+++ src/test/java/com/example/productorderservice/payment/PaymentApiTest.java
@@ -0,0 +1,31 @@
+package com.example.productorderservice.payment;
+
+import com.example.productorderservice.ApiTest;
+import com.example.productorderservice.order.OrderSteps;
+import com.example.productorderservice.product.ProductSteps;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+class PaymentApiTest extends ApiTest {
+
+    @Test
+    void 상품주문() {
+        ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
+        OrderSteps.상품주문요청(OrderSteps.상품주문요청_생성());
+        final PaymentRequest request = PaymentSteps.주문결제요청_생성();
+        final ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/payments")
+                .then()
+                .log()
+                .all().extract();
+        Assertions.assertEquals(response.statusCode(), HttpStatus.OK.value());
+    }
+}
--- src/test/java/com/example/productorderservice/payment/PaymentService.java
+++ src/test/java/com/example/productorderservice/payment/PaymentService.java
@@ -1,9 +1,15 @@
 package com.example.productorderservice.payment;
 
 import com.example.productorderservice.order.Order;
-import org.springframework.stereotype.Component;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
+@RequestMapping(value = "/payments")
 class PaymentService {
     private final PaymentPort paymentPort;
 
@@ -11,10 +17,12 @@ class PaymentService {
         this.paymentPort = paymentPort;
     }
 
-    public void payment(final PaymentRequest request) {
+    @PostMapping
+    public ResponseEntity<Void> payment(@RequestBody final PaymentRequest request) {
         final Order order = this.paymentPort.getOrder(request.orderId());
         final Payment payment = new Payment(order, request.cardNumber());
         this.paymentPort.pay(order.getTotalPrice(), payment.getCardNumber());
         this.paymentPort.save(payment);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
